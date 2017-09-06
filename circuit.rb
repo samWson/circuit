49b26bea4e66c5d664bcdecbd1904f88e7d89f45
Samuel Wilson <samWson@users.noreply.github.com>
2017-09-07 07:24:29 +1200

    Calculate the current from power and voltage

--- circuit.rb
+++ circuit.rb
@@ -5,7 +5,7 @@ class Circuit
   def initialize(power: nil, current: nil, voltage: nil)
 
     @power = (power == nil) ? calc_power(current: current, voltage: voltage) : power
-
+    @current = (current == nil) ? calc_current(power: power, voltage: voltage) : current
   end
 
   # Return the power(watts) given the current and voltage
@@ -14,11 +14,20 @@ class Circuit
   def calc_power(current:, voltage:)
 
     if current == nil or voltage == nil
-      raise ArgumentError, "One or more arguments are nil. Both current and voltage must have a value."
-    end
+      raise ArgumentError, "One or more arguments are nil. Both current and voltage must have a value."    end
 
-    current * voltage
-    
+    current * voltage    
   end
 
+  # Return the current(Amps) given the power and voltage
+  # power - measured in watts
+  # voltage - measured in volts
+  def calc_current(power:, voltage:)
+
+    if power == nil or voltage == nil
+      raise ArgumentError, "One or more arguments are nil. Both power and voltage must have a value."    end
+
+    power / voltage
+  end
+    
 end
